6aaba0603c281e16bad4c39c252c5cd5ec678ad8
Kristian Hellang <kristian@hellang.com>
2016-01-20 01:25:56 +0100

    Started splitting out stuff

---
+++ samples/WebApp/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Nancy.Bootstrapper.Prototype.Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+        }
+
+        public static void Main(string[] args)
+        {
+            WebApplication.Run<Startup>(args);
+        }
+    }
+}
