dbd54bb430e958233c5db84446aa298b7c17a5ae
Kristian Hellang <kristian@hellang.com>
2016-01-25 22:31:54 +0100

    Went back to net451

--- src/Nancy.Core/Registration/ContainerRegistry.cs
+++ src/Nancy.Core/Registration/ContainerRegistry.cs
@@ -1,17 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Nancy.Core.Registration
 {
     public class ContainerRegistry : IContainerRegistry
     {
+        private static readonly TypeRegistration[] EmptyTypeRegistrations = new TypeRegistration[0];
+
+        private static readonly CollectionTypeRegistration[] EmptyCollectionTypeRegistrations = new CollectionTypeRegistration[0];
+
+        private static readonly InstanceRegistration[] EmptyInstanceRegistrations = new InstanceRegistration[0];
+
         public ContainerRegistry(IReadOnlyCollection<TypeRegistration> typeRegistrations = null,
             IReadOnlyCollection<CollectionTypeRegistration> collectionTypeRegistrations = null,
             IReadOnlyCollection<InstanceRegistration> instanceRegistrations = null)
         {
-            TypeRegistrations = typeRegistrations ?? Array.Empty<TypeRegistration>();
-            CollectionTypeRegistrations = collectionTypeRegistrations ?? Array.Empty<CollectionTypeRegistration>();
-            InstanceRegistrations = instanceRegistrations ?? Array.Empty<InstanceRegistration>();
+            TypeRegistrations = typeRegistrations ?? EmptyTypeRegistrations;
+            CollectionTypeRegistrations = collectionTypeRegistrations ?? EmptyCollectionTypeRegistrations;
+            InstanceRegistrations = instanceRegistrations ?? EmptyInstanceRegistrations;
         }
 
         public IReadOnlyCollection<TypeRegistration> TypeRegistrations { get; }
