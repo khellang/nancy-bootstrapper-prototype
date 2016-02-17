95c4b4c94069390e7093f31dcfe6b0ff3a10f9c0
Kristian Hellang <kristian@hellang.com>
2016-02-17 15:57:23 +0100

    Kill extra method

--- src/Nancy.Core/Scanning/ScanningStrategies.cs
+++ src/Nancy.Core/Scanning/ScanningStrategies.cs
@@ -5,17 +5,12 @@
 
     public static class ScanningStrategies
     {
-        public static readonly Assembly NancyAssembly = typeof(IEngine).GetTypeInfo().Assembly;
+        private static readonly Assembly NancyAssembly = typeof(IEngine).GetTypeInfo().Assembly;
 
         public static ScanningStrategy All { get; } = assembly => true;
 
-        public static ScanningStrategy OnlyNancy { get; } = assembly => IsNancy(assembly);
+        public static ScanningStrategy OnlyNancy { get; } = assembly => assembly.Equals(NancyAssembly);
 
-        public static ScanningStrategy ExcludeNancy { get; } = assembly => !IsNancy(assembly);
-
-        private static bool IsNancy(Assembly assembly)
-        {
-            return assembly.Equals(NancyAssembly);
-        }
+        public static ScanningStrategy ExcludeNancy { get; } = assembly => !assembly.Equals(NancyAssembly);
     }
 }
