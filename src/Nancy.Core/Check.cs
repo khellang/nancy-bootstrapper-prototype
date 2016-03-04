30a26667e1f43284aa2419288a80c28681860172
Kristian Hellang <kristian@hellang.com>
2016-03-04 16:46:53 +0100

    Step through null checks

--- src/Nancy.Core/Check.cs
+++ src/Nancy.Core/Check.cs
@@ -1,7 +1,9 @@
 ﻿namespace Nancy.Core
 {
     using System;
+    using System.Diagnostics;
 
+    [DebuggerStepThrough]
     public static class Check
     {
         public static void NotNull<T>(T value, string paramName)
